39b3182a48b2e359e363ffc8e340a3d89179ceb5
Eugene Zagidullin <e.asphyx@gmail.com>
2021-05-26 22:17:03 +0300

    Disable colors

--- monitor.ts
+++ monitor.ts
@@ -1,8 +1,14 @@
 import * as flags from "https://deno.land/std@0.97.0/flags/mod.ts";
 import { MatrixMonitor, MonitorOptions } from "./matrix-monitor.ts";
 import * as log from "https://deno.land/std@0.97.0/log/mod.ts";
-import { ConsoleHandler } from "https://deno.land/std@0.97.0/log/handlers.ts";
+import { BaseHandler } from "https://deno.land/std@0.97.0/log/handlers.ts";
 import { LogRecord } from "https://deno.land/std@0.97.0/log/logger.ts";
+
+class LogHandler extends BaseHandler {
+    log(msg: string): void {
+        console.log(msg);
+    }
+}
 
 const args = flags.parse(Deno.args);
 
@@ -15,7 +21,7 @@ const opt: MonitorOptions = {
 // mimic a structured JSON logger
 await log.setup({
     handlers: {
-        default: new ConsoleHandler("INFO", {
+        default: new LogHandler("INFO", {
             formatter: (r: LogRecord) => {
                 const res = {
                     timestamp: r.datetime,
